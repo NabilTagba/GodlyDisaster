eec353e8a83affc4788b36d0781b7f6262b31bbd
rswansonbradley <raswanson@mail.bradley.edu>
2023-04-24 13:49:11 -0500

    ArrowManager

---
+++ Assets/Scripts/ArrowManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowManager : MonoBehaviour
+{
+    public GameObject UpArrow;
+    public GameObject DownArrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
